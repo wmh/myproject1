8ec8c32485d744ced924cd0f34f0b22ac53495ab
unknown <07396@.dlink.com.tw>
2010-03-12 15:05:29 +0800

    try some php functions

--- PHP/001_getopt.php
+++ PHP/001_getopt.php
@@ -1,5 +1,7 @@
 <?php
 
+//windows 要 5.3.0 以後才支援！
+
 //print_r(getopt($shortopts, $longopts));
 print_r(getopt("a:b:c:d:e:f:g:h:"));
 
---
+++ PHP/002_GLOBALS.php
@@ -0,0 +1,12 @@
+<?php
+
+$GLOBALS['test'] = 1;
+aa();
+
+
+function aa() {
+    echo $GLOBALS['test'], "\n";
+}
+
+
+?>
