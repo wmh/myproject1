797b28ff4171e189a4c9669d579f0b4bb0f49db3
HunterNB <hunter_wu@dlink.com.tw>
2010-04-30 09:35:22 +0800

    testing io

--- YUI3/io/01.php
+++ YUI3/io/01.php
@@ -1,6 +1,10 @@
 <?php
-
-
-
+//sleep(mt_rand(1, 1));
+echo json_encode(
+    array(
+        "result" => 1,
+        "msg" => ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'xmlhttprequest')
+    )
+);
 
 ?>
