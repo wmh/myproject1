d9730dc1a2317a49842bc13da58e17a8af556836
HunterNB <hunter_wu@dlink.com.tw>
2010-05-10 17:29:33 +0800

    test yui2 uploader

--- YUI2/Uploader/upload_simple.php
+++ YUI2/Uploader/upload_simple.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("log.txt", "=== ". date("Y-m-d H:i:s") ." ===". print_r($_FILES, TRUE) ."\n", FILE_APPEND);
+file_put_contents("log.txt", "=== ". date("Y-m-d H:i:s") ." ===". print_r($_FILES, TRUE) . print_r($_POST, TRUE) ."\n", FILE_APPEND);
 
 echo "ok";
 ?>
