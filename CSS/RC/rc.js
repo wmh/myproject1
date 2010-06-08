159c80dab8e3b2393cab95db4fd9fec1395b784e
HunterNB <hunter_wu@dlink.com.tw>
2010-06-08 09:15:23 +0800

    fixed for jslint

--- CSS/RC/rc.js
+++ CSS/RC/rc.js
@@ -1,38 +1,41 @@
+/*global YUI
+*/
 YUI().use("node", function (Y) {
-    if (Y.UA.ie && Y.UA.ie < 9) {
-        Y.all(".miii-rc5").each(function(node) {
-            var position, xy, w, h, rcTL, rcTR, rcBL, rcBR;
-            position = node.getStyle("position");
-
-            if (position === "relative" || position === "absolute") {
-                node.append('<span class="miii-rc5-decorations"><span class="miii-rc5-tl"></span><span class="miii-rc5-tr"></span><span class="miii-rc5-bl"></span><span class="miii-rc5-br"></span></span>');
-                return;
-            }
-            if (Y.UA.ie >= 7) {
-                node.setStyle("position", "relative").addClass("clearfix");
-                node.append('<span class="miii-rc5-decorations"><span class="miii-rc5-tl"></span><span class="miii-rc5-tr"></span><span class="miii-rc5-bl"></span><span class="miii-rc5-br"></span></span>');
-                return;
-            }
-            xy = node.getXY();
-            w = node.get('offsetWidth');
-            h = node.get('offsetHeight');
-            rcTL = Y.Node.create('<span class="miii-rc5-tl"></span>').setStyles({
-                top: xy[1],
-                left: xy[0]
-            });
-            rcTR = Y.Node.create('<span class="miii-rc5-tr"></span>').setStyles({
-                top: xy[1],
-                left: xy[0] + w - 5
-            });
-            rcBL = Y.Node.create('<span class="miii-rc5-bl"></span>').setStyles({
-                top: xy[1] + h - 5,
-                left: xy[0]
-            });
-            rcBR = Y.Node.create('<span class="miii-rc5-br"></span>').setStyles({
-                top: xy[1] + h - 5,
-                left: xy[0] + w - 5
-            });
-            node.append(Y.Node.create('<span class="miii-rc5-decorations"></span>').append(rcTL).append(rcTR).append(rcBL).append(rcBR));
-        });
+    if (!Y.UA.ie || Y.UA.ie >= 9) {
+        return;
     }
+    Y.all(".miii-rc5").each(function (node) {
+        var position, xy, w, h, rcTL, rcTR, rcBL, rcBR;
+        position = node.getStyle("position");
+
+        if (position === "relative" || position === "absolute") {
+            node.append('<span class="miii-rc5-decorations"><span class="miii-rc5-tl"></span><span class="miii-rc5-tr"></span><span class="miii-rc5-bl"></span><span class="miii-rc5-br"></span></span>');
+            return;
+        }
+        if (Y.UA.ie >= 7) {
+            node.setStyle("position", "relative").addClass("clearfix");
+            node.append('<span class="miii-rc5-decorations"><span class="miii-rc5-tl"></span><span class="miii-rc5-tr"></span><span class="miii-rc5-bl"></span><span class="miii-rc5-br"></span></span>');
+            return;
+        }
+        xy = node.getXY();
+        w = node.get('offsetWidth');
+        h = node.get('offsetHeight');
+        rcTL = Y.Node.create('<span class="miii-rc5-tl"></span>').setStyles({
+            top: xy[1],
+            left: xy[0]
+        });
+        rcTR = Y.Node.create('<span class="miii-rc5-tr"></span>').setStyles({
+            top: xy[1],
+            left: xy[0] + w - 5
+        });
+        rcBL = Y.Node.create('<span class="miii-rc5-bl"></span>').setStyles({
+            top: xy[1] + h - 5,
+            left: xy[0]
+        });
+        rcBR = Y.Node.create('<span class="miii-rc5-br"></span>').setStyles({
+            top: xy[1] + h - 5,
+            left: xy[0] + w - 5
+        });
+        node.append(Y.Node.create('<span class="miii-rc5-decorations"></span>').append(rcTL).append(rcTR).append(rcBL).append(rcBR));
+    });
 });
