c78fedd2e7d99697a2429152b58b0e7b811f06ef
Hunter Wu <hunter.wu@gmail.com>
2013-09-08 16:03:36 +0800

    Arduino AnalogWrite

---
+++ Arduino/AnalogWrite/AnalogWrite.ino
@@ -0,0 +1,16 @@
+int thisPin = 10;
+void setup() {
+  pinMode(thisPin, OUTPUT);
+}
+void loop() {
+  for (int brightness = 0; brightness < 255; brightness++) {
+    analogWrite(thisPin, brightness);
+    delay(2);
+  } 
+  // fade the LED on thisPin from brithstest to off:
+  for (int brightness = 255; brightness >= 0; brightness--) {
+    analogWrite(thisPin, brightness);
+    delay(1);
+  } 
+  delay(500);
+}
