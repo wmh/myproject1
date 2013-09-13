46ed250e76c375c0a7c53c9e1ea5d3748278b79d
Hunter Wu <hunter.wu@gmail.com>
2013-09-14 01:33:32 +0800

    My Arduino Toy

---
+++ Arduino/SerialPort/SerialPort.ino
@@ -0,0 +1,20 @@
+int thisPin = 10;
+int baudRate = 9600;
+void setup() {
+  pinMode(thisPin, OUTPUT);
+  Serial.begin(baudRate);
+}
+void loop() {
+  for (int brightness = 0; brightness < 255; brightness++) {
+    analogWrite(thisPin, brightness);
+    delay(2);
+  } 
+  // fade the LED on thisPin from brithstest to off:
+  for (int brightness = 255; brightness >= 0; brightness--) {
+    analogWrite(thisPin, brightness);
+    delay(1);
+  } 
+  Serial.println("End");
+  delay(1500);
+  Serial.println("Start");
+}
---
+++ Arduino/digitalRead/digitalRead.ino
@@ -0,0 +1,17 @@
+int switchPin=5;
+int ledPin=6;
+
+void setup() {
+  pinMode(5, INPUT);
+  pinMode(6, OUTPUT);
+}
+void loop() {
+  if (digitalRead(5) == LOW)
+  {
+    digitalWrite(6, HIGH);
+  }
+  else
+  {
+    digitalWrite(6, LOW);
+  }
+}
