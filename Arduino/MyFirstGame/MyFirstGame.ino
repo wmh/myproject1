f778c0e9b3940f1bb10a21e6442daf2dfd8ba337
Hunter Wu <hunter.wu@gmail.com>
2013-09-17 06:42:38 +0800

    Failed

---
+++ Arduino/MyFirstGame/MyFirstGame.ino
@@ -0,0 +1,19 @@
+int startPin = 7;
+int playerA = 8;
+int playerB = 11;
+
+int ledPin=13;
+int switchState=HIGH;
+int ledState=LOW;
+
+void setup() {
+  pinMode(startPin, INPUT);
+  pinMode(playerA, INPUT);
+  pinMode(playerB, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  digitalWrite(ledPin, digitalRead(startPin) == HIGH || digitalRead(playerA) == HIGH || digitalRead(playerB) == HIGH ? LOW : HIGH);
+  delay(50);
+
+}
