c19a7e7bc858a30b64c77ff21e6167defdcf298a
Hunter Wu <hunter.wu@gmail.com>
2013-09-22 03:05:01 +0800

    Add led to response to adjustable resistor

--- Arduino/AdjustableResistor/AdjustableResistor.ino
+++ Arduino/AdjustableResistor/AdjustableResistor.ino
@@ -1,9 +1,22 @@
+int ledPin = 11;
+int inNum;
+int outNum;
 void setup()
 {
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 void loop()
 {
-  Serial.println(analogRead(A0));
+  inNum = analogRead(A0);
+  Serial.println(inNum);
   delay(1000);
+}
+void loop2()
+{
+  inNum = analogRead(A0);
+  outNum = (int)(analogRead(A0) / 4);
+  Serial.println(inNum);
+  analogWrite(ledPin, outNum);
+  delay(100);
 }
