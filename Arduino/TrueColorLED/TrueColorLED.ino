e897eb7a98e4b165e3cdccc8ebae642126d9136b
Hunter Wu <hunter.wu@gmail.com>
2014-01-16 23:42:45 +0800

    key move

--- Arduino/TrueColorLED/TrueColorLED.ino
+++ Arduino/TrueColorLED/TrueColorLED.ino
@@ -1,6 +1,6 @@
-int R = 9;
-int G = 11;
+int R = 11;
 int B = 10;
+int G = 9;
 
 void setup() {
   pinMode(R, OUTPUT);
@@ -9,14 +9,16 @@ void setup() {
 }
 void loop() {
   for (int brightness = 0; brightness < 255; brightness++) {
-    analogWrite(R, brightness);
+    analogWrite(R, brightness / 2);
     analogWrite(G, brightness);
+    analogWrite(B, brightness);
     delay(2);
   } 
   // fade the LED on thisPin from brithstest to off:
   for (int brightness = 255; brightness >= 0; brightness--) {
-    analogWrite(R, brightness);
+    analogWrite(R, brightness / 2);
     analogWrite(G, brightness);
+    analogWrite(B, brightness);
     delay(1);
   } 
   delay(500);
